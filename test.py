ecc38c5fe8af8e6a0b71f2ea6ca22ce20870aa7d
IntModZero <intmodzero@gmail.com>
2017-07-04 16:30:38 -0400

    added canny edge

--- test.py
+++ test.py
@@ -2,8 +2,19 @@ import cv2
 import numpy as np
 from matplotlib import pyplot
 
+
+def nothing(x):
+    pass
+
 img = cv2.imread('imgs/01.jpg',0)
 
-cv2.imshow("image",img)
+# blur to reduce noise
+img = cv2.GaussianBlur(img,(5,5),0)
+
+# canny edge detection on img
+cannyImg = cv2.Canny(img,120,220)
+
+
+cv2.imshow("canny edged image",cannyImg)
 cv2.waitKey(0)
 cv2.destroyAllWindows() 
