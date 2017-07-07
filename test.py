c72481a114a2bb4bcdc9ea1bac7cc7434e5ef6d1
IntModZero <intmodzero@gmail.com>
2017-07-07 15:08:36 -0400

    close circle

--- test.py
+++ test.py
@@ -3,18 +3,18 @@ import numpy as np
 from matplotlib import pyplot
 
 
-def nothing(x):
-    pass
-
 img = cv2.imread('imgs/01.jpg',0)
 
 # blur to reduce noise
 img = cv2.GaussianBlur(img,(5,5),0)
 
 # canny edge detection on img
-cannyImg = cv2.Canny(img,120,220)
+cannyImg = cv2.Canny(img,100,170)
 
+#morph
+kernel = np.ones((3, 3), np.uint8)
+close = cv2.morphologyEx(cannyImg,cv2.MORPH_CLOSE,kernel, iterations=5)
 
-cv2.imshow("canny edged image",cannyImg)
+cv2.imshow("canny edged image",close)
 cv2.waitKey(0)
 cv2.destroyAllWindows() 
